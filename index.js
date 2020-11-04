576baccea0f3c5e6c08dc71ba8445c0b1a8a9a02
Dorian Storella <dorianstorella@gmail.com>
2020-11-04 10:10:53 +0100

    "premier script"

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-index.js
+//ma api kley a étée utilisée dans  navigateur a la suite de:
+// https://api.themoviedb.org/3/movie/76341?api_key=
+const API_KEY = 'a5e9ce910863d3be9153c337678e8460';
+// jeton v4 auth 
+//eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNWU5Y2U5MTA4NjNkM2JlOTE1M2MzMzc2NzhlODQ2MCIsInN1YiI6IjVmYTE1ZDg1MGMxMjU1MDAzNzI3N2MxMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wh2Xh_ffGQ2GWqZ9-W3DHMva1VEAkXSFP9UwXXPGNoo
+
+  //--url 'https://api.themoviedb.org/3/movie/76341' \
+  //--header 'Authorization: Bearer <<access_token>>' \
+  //--header 'Content-Type: application/json;charset=utf-8'
+
+const endpoint='https://api.themoviedb.org/3/search/movie?api_key=a5e9ce910863d3be9153c337678e8460&query=furious&page=2'
+
+//select element DOM
+const inputElement = document.querySelector('#inputValue');
+const button = document.querySelector('#search')
+button.onclick = function(event){
+  event.preventDefault();
+  const value = inputElement.value;
+  console.log('value:' ,value)
+} 
